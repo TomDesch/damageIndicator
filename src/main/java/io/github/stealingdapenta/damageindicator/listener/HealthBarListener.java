a8c96e39b6b8f3e6cc5be40b2e98e530fea5b492
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2023-12-04 16:41:47 +0100

    005_edit_bar_style : HealthBar event priority + edit styles!

--- src/main/java/io/github/stealingdapenta/damageindicator/listener/HealthBarListener.java
+++ src/main/java/io/github/stealingdapenta/damageindicator/listener/HealthBarListener.java
@@ -5,11 +5,11 @@ import io.github.stealingdapenta.damageindicator.DamageIndicator;
 import io.github.stealingdapenta.damageindicator.DefaultConfigValue;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -18,17 +18,23 @@ import org.bukkit.scheduler.BukkitTask;
 import java.util.HashMap;
 import java.util.Objects;
 
+import static io.github.stealingdapenta.damageindicator.DefaultConfigValue.HEALTH_BAR_ALIVE_COLOR;
+import static io.github.stealingdapenta.damageindicator.DefaultConfigValue.HEALTH_BAR_ALIVE_SYMBOL;
+import static io.github.stealingdapenta.damageindicator.DefaultConfigValue.HEALTH_BAR_DEAD_COLOR;
+import static io.github.stealingdapenta.damageindicator.DefaultConfigValue.HEALTH_BAR_DEAD_SYMBOL;
+import static io.github.stealingdapenta.damageindicator.DefaultConfigValue.HEALTH_BAR_STRIKETHROUGH;
+import static io.github.stealingdapenta.damageindicator.DefaultConfigValue.HEALTH_BAR_UNDERLINED;
+
 public class HealthBarListener implements Listener {
     private static final int TICKS_PER_SECOND = 20;
     private static final int MIN_SECONDS = 1;
     private static final int MAX_SECONDS = 10;
-    private static final String SPACE = " ";
     private final HashMap<LivingEntity, BukkitTask> entitiesWithActiveHealthBars = new HashMap<>();
     private final HashMap<LivingEntity, Component> originalEntityNames = new HashMap<>();
-    private final ConfigurationFileManager configurationFileManager;
+    private final ConfigurationFileManager cfm;
 
     public HealthBarListener() {
-        configurationFileManager = ConfigurationFileManager.getInstance();
+        cfm = ConfigurationFileManager.getInstance();
     }
 
     private Component createHealthBar(double currentHealth, double maxHealth) {
@@ -36,17 +42,37 @@ public class HealthBarListener implements Listener {
         int aliveBarLength = Math.max(0, (int) (percentAlive * 16));
         int deadBarLength = Math.max(0, 16 - aliveBarLength);
 
-        TextComponent aliveComponent = getBoldAndStrikeThroughSpaceLine(aliveBarLength, configurationFileManager.getTextColor(DefaultConfigValue.HEALTH_BAR_ALIVE_COLOR));
-        TextComponent deadComponent = getBoldAndStrikeThroughSpaceLine(deadBarLength, configurationFileManager.getTextColor(DefaultConfigValue.HEALTH_BAR_DEAD_COLOR));
+        TextComponent aliveComponent = buildAliveComponent(aliveBarLength);
+        TextComponent deadComponent = buildDeadComponent(deadBarLength);
 
-        return aliveComponent.append(deadComponent);
+        return applyConfigStyles(aliveComponent.append(deadComponent));
     }
 
-    private TextComponent getBoldAndStrikeThroughSpaceLine(int barLength, TextColor color) {
-        return Component.text(SPACE.repeat(barLength), color, TextDecoration.STRIKETHROUGH, TextDecoration.BOLD);
+    private TextComponent buildAliveComponent(int barLength) {
+        return Component.text(cfm.getStringValue(HEALTH_BAR_ALIVE_SYMBOL).repeat(barLength), cfm.getTextColor(HEALTH_BAR_ALIVE_COLOR));
     }
 
-    @EventHandler
+    private TextComponent buildDeadComponent(int barLength) {
+        return Component.text(cfm.getStringValue(HEALTH_BAR_DEAD_SYMBOL).repeat(barLength), cfm.getTextColor(HEALTH_BAR_DEAD_COLOR));
+    }
+
+    private Component applyConfigStyles(TextComponent healthBar) {
+        if (cfm.getBooleanValue(DefaultConfigValue.HEALTH_BAR_BOLD)) {
+            healthBar = healthBar.decorate(TextDecoration.BOLD);
+        }
+
+        if (cfm.getBooleanValue(HEALTH_BAR_STRIKETHROUGH)) {
+            healthBar = healthBar.decorate(TextDecoration.STRIKETHROUGH);
+        }
+
+        if (cfm.getBooleanValue(HEALTH_BAR_UNDERLINED)) {
+            healthBar = healthBar.decorate(TextDecoration.UNDERLINED);
+        }
+
+        return healthBar;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
     public void displayHealthBar(EntityDamageEvent event) {
         if (!(event.getEntity() instanceof LivingEntity livingEntity)) return;
 
@@ -75,7 +101,7 @@ public class HealthBarListener implements Listener {
     }
 
     private int getTicksDuration() {
-        int displayDuration = configurationFileManager.getValue(DefaultConfigValue.HEALTH_BAR_DISPLAY_DURATION);
+        int displayDuration = cfm.getValue(DefaultConfigValue.HEALTH_BAR_DISPLAY_DURATION);
         return TICKS_PER_SECOND * Math.max(MIN_SECONDS, Math.min(displayDuration, MAX_SECONDS));
     }
 
