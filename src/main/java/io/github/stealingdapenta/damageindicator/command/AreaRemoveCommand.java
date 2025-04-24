68715ba36c0e503452a35259c15dc75783c7b3d6
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2025-04-24 18:52:52 +0200

    Refactor AreaRemoveCommand to improve readability and performance

--- src/main/java/io/github/stealingdapenta/damageindicator/command/AreaRemoveCommand.java
+++ src/main/java/io/github/stealingdapenta/damageindicator/command/AreaRemoveCommand.java
@@ -3,45 +3,44 @@ package io.github.stealingdapenta.damageindicator.command;
 import static io.github.stealingdapenta.damageindicator.listener.DamageIndicatorListener.getCustomNamespacedKey;
 
 import io.github.stealingdapenta.damageindicator.config.Permission;
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
 public class AreaRemoveCommand implements CommandExecutor {
 
-    private static final String NO_PERMISSION = "You don't have the required %s to execute this command.";
-    private static final String RELOADED = "Successfully removed %s nearby entities.";
+    private static final int RADIUS = 50;
+    private static final String NO_PERMISSION_MSG = "You don't have the required %s to execute this command.";
+    private static final String REMOVED_MSG = "Successfully removed %s nearby entities.";
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender,
-                             @NotNull Command command,
-                             @NotNull String label,
-                             String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String @NotNull [] args) {
 
         if (!(sender instanceof Player player)) {
-            return false;
-        }
-
-        if (!sender.hasPermission(Permission.AREA_REMOVE.getNode())) {
-            sender.sendMessage(NO_PERMISSION.formatted(Permission.AREA_REMOVE.getNode()));
+            sender.sendMessage("This command can only be used by players.");
             return true;
         }
 
-        int counter = player.getNearbyEntities(50, 50, 50)
-                            .stream()
-                            .filter(entity -> Boolean.TRUE.equals(entity.getPersistentDataContainer()
-                                                                        .getOrDefault(getCustomNamespacedKey(), PersistentDataType.BOOLEAN, false)))
-                            .mapToInt(entity -> {
-                                entity.remove();
-                                return 1;
-                            })
-                            .sum();
+        if (!player.hasPermission(Permission.AREA_REMOVE.getNode())) {
+            player.sendMessage(Component.text(NO_PERMISSION_MSG.formatted(Permission.AREA_REMOVE.getNode())));
+            return true;
+        }
 
-        sender.sendMessage(RELOADED.formatted(counter));
+        int removedCount = 0;
+        for (Entity entity : player.getNearbyEntities(RADIUS, RADIUS, RADIUS)) {
+            if (Boolean.TRUE.equals(entity.getPersistentDataContainer()
+                                          .getOrDefault(getCustomNamespacedKey(), PersistentDataType.BOOLEAN, false))) {
+                entity.remove();
+                removedCount++;
+            }
+        }
 
+        player.sendMessage(Component.text(REMOVED_MSG.formatted(removedCount)));
         return true;
     }
 }
