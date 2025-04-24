951626805444dbcd6e122ee7e454ab97061b16fc
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2025-04-24 18:53:33 +0200

    Refactor ReloadConfigCommand to enhance permission handling and message formatting

--- src/main/java/io/github/stealingdapenta/damageindicator/command/ReloadConfigCommand.java
+++ src/main/java/io/github/stealingdapenta/damageindicator/command/ReloadConfigCommand.java
@@ -2,6 +2,7 @@ package io.github.stealingdapenta.damageindicator.command;
 
 import io.github.stealingdapenta.damageindicator.config.ConfigurationFileManager;
 import io.github.stealingdapenta.damageindicator.config.Permission;
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,23 +10,28 @@ import org.jetbrains.annotations.NotNull;
 
 public class ReloadConfigCommand implements CommandExecutor {
 
-    private static final String NO_PERMISSION = "You don't have the required %s to execute this command.";
-    private static final String RELOADED = "Successfully reloaded the DamageIndicator configuration file.";
+    private static final String NO_PERMISSION_MSG = "You don't have the required %s to execute this command.";
+    private static final String RELOADED_MSG = "Successfully reloaded the DamageIndicator configuration file.";
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender,
-                             @NotNull Command command,
-                             @NotNull String label,
-                             String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String @NotNull [] args) {
 
-        if (!sender.hasPermission(Permission.RELOAD.getNode())) {
-            sender.sendMessage(NO_PERMISSION.formatted(Permission.RELOAD.getNode()));
+        if (!hasPermissionOrWarn(sender, Permission.RELOAD.getNode())) {
             return true;
         }
 
         ConfigurationFileManager.getInstance()
                                 .reloadConfig();
-        sender.sendMessage(RELOADED);
+        sender.sendMessage(Component.text(RELOADED_MSG));
         return true;
+    }
+
+    private boolean hasPermissionOrWarn(CommandSender sender, String permission) {
+        if (sender.hasPermission(permission)) {
+            return true;
+        }
+
+        sender.sendMessage(Component.text(NO_PERMISSION_MSG.formatted(permission)));
+        return false;
     }
 }
